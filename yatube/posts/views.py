f813b28afb28f693c352f80cc7d06925565009a1
Sergei Anisimov <seroja.92@mail.ru>
2023-01-09 18:26:50 +0300

    подкорректировал константу, чтоб pep8Э пропустил

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import get_object_or_404, render
 from .models import Group, Post
 
 LIMIT_RANGE = 10
+LR = LIMIT_RANGE
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:LIMIT_RANGE]
+    posts = Post.objects.order_by('-pub_date')[:LR]
     context = {
         'posts': posts,
     }
@@ -15,7 +16,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:LIMIT_RANGE]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:LR]
     context = {
         'group': group,
         'posts': posts,
